cb31a4269fd45598952cba3e48a5d7222ac0d914
crimolf <alupo662299@gmail.com>
2023-08-10 11:32:28 +0200

    primo rilascio

---
+++ src/main/java/it/fivenet/library/LibraryApplication.java
@@ -0,0 +1,13 @@
+package it.fivenet.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryApplication.class, args);
+    }
+
+}
