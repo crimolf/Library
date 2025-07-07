6cff86880d8fff144e43c13d70dbc5febd9eebaf
Alessandro Lupo <alessandro.lupo@fivenet.it>
2025-07-07 22:36:21 +0200

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'; // ✅ Needed for <router-outlet>
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateBookComponent } from './create-book/create-book.component';
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { BookListComponent } from './book-list/book-list.component';
-import { HttpClientModule } from '@angular/common/http';
-import {CreateOrderComponent} from "./create-order/create-order.component";
-import {OrderDetailsComponent} from "./order-details/order-details.component";
-import {OrderListComponent} from "./order-list/order-list.component";
+import { CreateOrderComponent } from './create-order/create-order.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
+import { OrderListComponent } from './order-list/order-list.component';
 import { UpdateOrderComponent } from './update-order/update-order.component';
 import { UpdateBookComponent } from './update-book/update-book.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialogModule} from "@angular/material/dialog";
-import {MatIconModule} from "@angular/material/icon";
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -23,17 +26,15 @@ import {MatIconModule} from "@angular/material/icon";
     BookDetailsComponent,
     BookListComponent,
     CreateOrderComponent,
-
     OrderDetailsComponent,
     OrderListComponent,
-    BookDetailsComponent,
-    BookListComponent,
     UpdateOrderComponent,
     UpdateBookComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule, // ✅ Add this so <router-outlet> works
     FormsModule,
     HttpClientModule,
     MatButtonModule,
